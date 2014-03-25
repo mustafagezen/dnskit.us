dfdfbf1470818c41326f361eaf77b8c5f408b846
Mustafa Gezen <mustafa@norwaykarate.no>
2014-03-25 18:05:29 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -13,4 +13,7 @@ $(document).ready(function(){
       $("dns").append("<p><b>" + dns[key][0] + "</b>, <b>" + dns[key][1] + "</b> - " + dns[key][2] + "</p>");
     }
   }
+  
+  $("#mdns").html("<b>" + dns[main_dns][0] + "</b>, <b>" + dns[main_dns][1] + "</b>");
+  $("#name").html("<b>" + dns[main_dns][2]);
 });
