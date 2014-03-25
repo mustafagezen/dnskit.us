f0df4b9d5a903221d514874a5f52ea6e47c8bd27
Mustafa Gezen <mustafa@norwaykarate.no>
2014-03-25 23:12:27 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,3 @@
-var dns = {}
-dns.google = new Array("8.8.8.8", "8.8.4.4", "Google Public DNS Server");
-dns.opendns = new Array("208.67.222.222", "208.67.220.220", "OpenDNS");
-dns.nortondns = new Array("198.153.192.50", "198.153.194.50", "Norton DNS Server");
-dns.advantage = new Array("156.154.70.1", "156.154.71.1", "DNS Advantage");
-dns.comodo = new Array("8.26.56.26", "8.20.247.20", "Comodo Secure DNS Server");
-
-var main_dns = "google";
-
 $(document).ready(function(){
   if($("dns")){
     for(var key in dns){
