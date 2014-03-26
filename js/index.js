833e288ccce7fad18f7607177af92c0763d133a7
Mustafa Gezen <mustafa@norwaykarate.no>
2014-03-26 17:03:18 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   if($("dns")){
+    $(".twitter-timeline").height(350)
     for(var key in dns){
       $("dns").append("<p><b>" + dns[key][0] + "</b>, <b>" + dns[key][1] + "</b> - " + dns[key][2] + "</p>");
     }
